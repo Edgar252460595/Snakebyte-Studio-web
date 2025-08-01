3984fd1131f3bb9c37ee1a52b2da8cdf66ea8441
Edgar252460595 <edgar.hernandez3534@gmail.com>
2025-08-01 01:04:40 -0500

    agregar pagina calculadora mecanica y arreglos en diseño inicial

---
+++ Proyectos/calculadora/js/script.js
@@ -0,0 +1,43 @@
+const boton = document.querySelector('.BotonListaCalculadora');
+const listaCalculadora = document.getElementById('listaCalculadora');
+const enlaces = document.querySelectorAll('.listaCalculadora a');
+const calculadoras = document.querySelectorAll('.calculadora');
+
+
+//menu de eleccion de calculadora
+
+if (boton && listaCalculadora) {
+  boton.addEventListener('click', () => {
+    listaCalculadora.classList.toggle('activo');
+  });
+
+  document.addEventListener('click', (e) => {
+    if (!boton.contains(e.target) && !listaCalculadora.contains(e.target)) {
+      listaCalculadora.classList.remove('activo');
+    }
+  });
+}
+
+
+  // Manejo de enlaces del menú de calculadoras
+
+  enlaces.forEach(enlace => {
+    enlace.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      // Ocultar todas
+      calculadoras.forEach(calc => {
+        calc.style.display = 'none';
+      });
+
+      // Mostrar la seleccionada
+      const id = enlace.getAttribute('data-calculadora');
+      const mostrar = document.getElementById(`calc-${id}`);
+      if (mostrar) {
+        mostrar.style.display = 'block';
+      }
+
+      // (Opcional) cerrar el menú
+      document.getElementById('submenu').classList.remove('activo');
+    });
+  });
